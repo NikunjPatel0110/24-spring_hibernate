3b16453ec6ad60a85b6fba5898c73a223bf1cf36
Nikunj Patel <nikunjppatel3@gmail.com>
2024-08-16 16:50:56 +0530

    repository

---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //create table
+@Table(name="users")
+public class UserEntity 
+{
+	@Id //primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //auto increment the value of primary key
+	private Integer userId; 
+	private String firstname;
+	private String email;
+	private String password;
+	
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
