30a05cc379d8477c28e625b118ec0a8c99d845c0
Nikunj Patel <nikunjppatel3@gmail.com>
2024-08-20 18:05:34 +0530

    product entity

--- src/main/java/com/controller/RestaurantController.java
+++ src/main/java/com/controller/RestaurantController.java
@@ -1,12 +1,26 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.entity.RestaurantEntity;
+import com.repository.RestaurantRepository;
 
 @Controller
 public class RestaurantController {
+	
+	@Autowired
+	RestaurantRepository restaurantRepository;
+	
 @GetMapping("newrestaurant")
 public String newRestaurant(){
 	return "NewRestaurant";
+}
+@PostMapping
+public String saverestraunt(RestaurantEntity restaurantEntity) {
+	restaurantRepository.save(restaurantEntity);
+	return "Success";
 }
 }
